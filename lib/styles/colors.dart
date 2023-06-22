2c645cde0ab285fc3f026645f7f2cf535af93a89
White-Patrasche <5338095@gmail.com>
2023-06-22 17:23:05 +0900

    feat: init color styles

---
+++ lib/styles/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ColorSystem {
+  static const Color primary = Colors.blue;
+  static const Color secondary = Colors.amber;
+  static const Color background = Colors.white;
+}
---
+++ lib/styles/styles.dart
@@ -0,0 +1,3 @@
+library styles;
+
+export 'colors.dart';
